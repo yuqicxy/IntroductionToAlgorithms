4b41fd8ef5506ee05f4dbbdedfa4f4652cc21d15
Qi Yu <yuqicxy@users.noreply.github.com>
2018-08-12 01:33:39 +0800

    随机排列数组
    1.permute by sort
    2.randomize in place

--- include/RandomRangeArray/RandomRangeArray.h
+++ include/RandomRangeArray/RandomRangeArray.h
@@ -1,14 +1,34 @@
 #pragma once
 #include <vector>
+#include <random>
 
 template<typename T>
 bool PermuteBySorting(std::vector<T>& input)
 {
-
+	int length = input.size();
+	for (unsigned int i = 0; i < input.size(); ++i)
+	{
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<> dis(0, std::pow(length, 3));
+		input.at(i) = dis.gen();
+	}
+	return true;
 }
 
 template<typename T>
 bool RandomizeInPlace(std::vector<T>& input)
 {
-
+	unsigned int length = input.size();
+	for (unsigned int i = 0; i < length; ++i)
+	{
+		std::random_device rd;
+		std::mt19937 gen(rd());
+		std::uniform_int_distribution<> dis(i, length - 1);
+		unsigned int index = dis(gen);
+		T tmp = input.at(i);
+		input.at(i) = input.at(index);
+		input.at(index) = tmp;
+	}
+	return true;
 }
--- src/RandomRangeArray/RandomRangeArray.cpp
+++ src/RandomRangeArray/RandomRangeArray.cpp
@@ -5,5 +5,18 @@
 
 int main()
 {
+	std::vector<int> input;
+	for (unsigned int i = 1;i <= 10; ++i)
+	{
+		input.push_back(i);
+	}
+
+	RandomizeInPlace(input);
+
+	for (int i = 0;i < input.size(); ++i)
+	{
+		std::cout << input.at(i) << "\t";
+	}
+
 	return 0;
 }
